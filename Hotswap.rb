49b850baa603186b95a71815cd55c9f760cd6e74
Maumagnaguagno <maumagnaguagno@gmail.com>
2026-01-31 11:33:46 -0300

    Ignore zero temperature

--- Hotswap.rb
+++ Hotswap.rb
@@ -9,17 +9,14 @@
 filename, hotend_temperature, bed_temperature = ARGV
 raise 'Missing temperatures' unless hotend_temperature and bed_temperature
 gcode = File.read(filename)
-gcode.gsub!(/^M(104|109|140|190) S(\d+)/) {|l|
-  if $2 != '0'
-    n = case $1
-    when '104' then "M104 S#{hotend_temperature}" # Set hotend temperature
-    when '109' then "M109 S#{hotend_temperature}" # Wait for hotend temperature
-    when '140' then "M140 S#{bed_temperature}" # Set bed temperature
-    when '190' then "M190 S#{bed_temperature}" # Wait for bed temperature
-    end
-    puts "Replace '#{l}' with '#{n}'"
-    n
-  else "M#{$1} S0"
+gcode.gsub!(/^M(104|109|140|190) S([^0]\d*)/) {|l|
+  n = case $1
+  when '104' then "M104 S#{hotend_temperature}" # Set hotend temperature
+  when '109' then "M109 S#{hotend_temperature}" # Wait for hotend temperature
+  when '140' then "M140 S#{bed_temperature}" # Set bed temperature
+  when '190' then "M190 S#{bed_temperature}" # Wait for bed temperature
   end
+  puts "Replace '#{l}' with '#{n}'"
+  n
 }
 File.write(filename.sub(/\.gcode$/, "_h#{hotend_temperature}_b#{bed_temperature}.gcode"), gcode)
