f7804e6ae0ccefbbb111fd0aaa9190585e7beb9d
Maumagnaguagno <maumagnaguagno@gmail.com>
2026-02-09 18:10:28 -0300

    Support batch processing

--- Hotswap.rb
+++ Hotswap.rb
@@ -7,18 +7,20 @@
 #-----------------------------------------------
 
 if ARGV[0] == '-h'
-  puts 'ruby Hotswap.rb filename hotend_temperature bed_temperature'
+  puts 'ruby Hotswap.rb hotend_temperature bed_temperature {filenames}'
 else
-  filename, hotend_temperature, bed_temperature = ARGV
-  abort 'Missing temperature' unless hotend_temperature and bed_temperature
-  gcode = File.read(filename)
-  gcode.gsub!(/^M(104|109|140|190)(.*)S(?!0)\d+/) {|l|
-    n = case $1
-    when '104','109' then "M#{$1}#{$2}S#{hotend_temperature}" # Set/wait hotend temperature
-    when '140','190' then "M#{$1}#{$2}S#{bed_temperature}" # Set/wait bed temperature
-    end
-    puts "Replace '#{l}' with '#{n}'"
-    n
+  abort 'Expected integer in hotend temperature' unless (hotend_temperature = ARGV.shift).match?(/^\d+$/)
+  abort 'Expected integer in bed temperature' unless (bed_temperature = ARGV.shift).match?(/^\d+$/)
+  ARGV.each {|filename|
+    gcode = File.read(filename)
+    gcode.gsub!(/^M(104|109|140|190)(.*)S(?!0)\d+/) {|l|
+      n = case $1
+      when '104','109' then "M#{$1}#{$2}S#{hotend_temperature}" # Set/wait hotend temperature
+      when '140','190' then "M#{$1}#{$2}S#{bed_temperature}" # Set/wait bed temperature
+      end
+      puts "Replace '#{l}' with '#{n}'"
+      n
+    }
+    File.write(filename.sub(/\.gcode$/, "_h#{hotend_temperature}_b#{bed_temperature}.gcode"), gcode)
   }
-  File.write(filename.sub(/\.gcode$/, "_h#{hotend_temperature}_b#{bed_temperature}.gcode"), gcode)
 end
--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Hotswap
-**Swap hotend and bed temperature of G-code**
+**Swap hotend and bed temperatures of G-code**
 
 In 3D printing one can easily swap the material to make a new piece, but temperatures need to swap too.
 This program affects the commands M104, M109, M140 and M190 of [G-Code](https://en.wikipedia.org/wiki/G-code).
-The input file remains untouched, and a new file is generated with the new temperatures.
+The input files remains untouched, and new files are generated with the new temperatures.
 
 ## Execution
 
 ```Shell
-ruby Hotswap.rb filename hotend_temperature bed_temperature
+ruby Hotswap.rb hotend_temperature bed_temperature {filenames}
 ```
