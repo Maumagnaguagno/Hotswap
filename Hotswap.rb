02a0633e0bf86fec9a4e1dd6c63d10e44d2c42d5
Maumagnaguagno <maumagnaguagno@gmail.com>
2026-02-14 11:47:02 -0300

    Move S to second match

--- Hotswap.rb
+++ Hotswap.rb
@@ -13,10 +13,10 @@ else
   abort 'Expected integer in bed temperature' unless (bed_temperature = ARGV.shift).match?(/^\d+$/)
   (ARGV.empty? ? Dir.glob('*.gcode') : ARGV).each {|filename|
     puts filename
-    (gcode = File.read(filename)).gsub!(/^M(104|109|140|190)(.*)S(?!0)\d+/) {|l|
+    (gcode = File.read(filename)).gsub!(/^M(104|109|140|190)(.*S)(?!0)\d+/) {|l|
       n = case $1
-      when '104','109' then "M#{$1}#{$2}S#{hotend_temperature}" # Set/wait hotend temperature
-      when '140','190' then "M#{$1}#{$2}S#{bed_temperature}" # Set/wait bed temperature
+      when '104','109' then "M#$1#$2#{hotend_temperature}" # Set/wait hotend temperature
+      when '140','190' then "M#$1#$2#{bed_temperature}" # Set/wait bed temperature
       end
       puts "  Replace '#{l}' with '#{n}'"
       n
